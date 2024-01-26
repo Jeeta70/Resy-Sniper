558db644883677ef2c8d7ac55339c6a7bac033a4
Ajeet <ajeet.singh@smartinfocare.com>
2024-01-26 20:50:48 +0530

    add space

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Router />
+        <Router /> 
       </BrowserRouter>
     </QueryClientProvider>
   );
