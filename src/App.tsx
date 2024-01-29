2b0caff1d57f3c56c5fef55f8983d861f4e24e1c
Ajeet <ajeet.singh@smartinfocare.com>
2024-01-29 14:59:44 +0530

    create build

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter> 
         <Router /> 
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
