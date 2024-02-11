3f6e6b3bf131219a6dbfb3ffb52502cb7032d7e9
dexterrenick <dexter.renick@gmail.com>
2024-02-10 20:54:04 -0500

    Test

--- src/pages/newpage/index.tsx
+++ src/pages/newpage/index.tsx
@@ -241,7 +241,7 @@ const NewPage = () => {
                                     onClick={() => scrollToElement('how-it-works')}
                                 >
                                     {" "}
-                                    How it Works
+                                    TEST
                                 </p>
                                 <p
                                     className="hover:text-[#D62637] cursor-pointer"
